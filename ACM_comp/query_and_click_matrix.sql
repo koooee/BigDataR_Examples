dfd5b81c1e47e7007e0e841d0f508c742b27461f
koooee <nick@winthropgaming.com>
2012-08-18 04:23:37 +0000

    create query and click matrix

---
+++ ACM_comp/query_and_click_matrix.sql
@@ -0,0 +1,15 @@
+create table click_matrix as 
+select 
+       userid_id
+       ,sku_id
+       ,count(*) 
+from big_data_train_ids 
+group by userid_id, sku_id;
+
+create table query_matrix as 
+select 
+       query_id
+       ,sku_id
+       ,count(*) 
+from big_data_train_ids 
+group by query_id, sku_id;
