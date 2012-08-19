bbe110e68c0e267de8aefc19133274232d495fdc
koooee <nick@winthropgaming.com>
2012-08-19 03:02:58 +0000

    commented out non needed id conversion

--- ACM_comp/step2_convert_raw_ids_to_integers/runme.sh
+++ ACM_comp/step2_convert_raw_ids_to_integers/runme.sh
@@ -1,5 +1,5 @@
-(psql -f convert_raw_to_id.sql acm 2>&1) > /dev/null
-if [ $? -eq 0 ]; then echo "Converted RawIDs to IntegerIDs on train data"; else echo "Failed to convert IDs; email: nick@bigdatarlinux.com"; fi
+# (psql -f convert_raw_to_id.sql acm 2>&1) > /dev/null
+# if [ $? -eq 0 ]; then echo "Converted RawIDs to IntegerIDs on train data"; else echo "Failed to convert IDs; email: nick@bigdatarlinux.com"; fi
 
 (psql -f map_big_data_test.sql acm 2>&1) > /dev/null
 if [ $? -eq 0 ]; then echo "Converted RawIDs to IntegerIDs on test data"; else echo "Failed to convert IDs;  email: nick@bigdatarlinux.com"; fi
