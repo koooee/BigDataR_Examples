2e743b024f676865cd63327c3497a2e815e385bc
koooee <nick@winthropgaming.com>
2012-08-19 02:45:32 +0000

    changed default postgres log

--- ACM_comp/step1_load_data_into_postgres/runme.sh
+++ ACM_comp/step1_load_data_into_postgres/runme.sh
@@ -1,3 +1,3 @@
 # run postgres service in the background keeping a log in .pgsql.log
-((postgres -D /mnt/data 2>&1) > .pgsql.log) &
+((postgres -D /mnt/data 2>&1) > ~/.pgsql.log) &
 if [ $? -eq 0 ]; then echo "Yeah! postgres is running!"; else echo ":-( postgres failed to start. email nick@bigdatarlinux.com"; fi
