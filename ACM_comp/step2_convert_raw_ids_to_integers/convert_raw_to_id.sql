432cfd967fc8bbbd7370aeb0260ab3e3c551ffc1
koooee <nick@winthropgaming.com>
2012-08-23 17:46:15 +0000

    too much cleaning

--- ACM_comp/step2_convert_raw_ids_to_integers/convert_raw_to_id.sql
+++ ACM_comp/step2_convert_raw_ids_to_integers/convert_raw_to_id.sql
@@ -36,11 +36,5 @@ where
 
 -- clean up
 drop table big_data_train_userid_mapping;
-drop table big_data_train_sku_mapping;
 drop table big_data_train_category_mapping;
 drop table big_data_train_query_mapping;
-drop table big_data_train_category_mapping;
-drop table big_data_train_query_mapping;
-drop table big_data_train_sku_mapping;
-drop table big_data_train_userid_mapping;
-
